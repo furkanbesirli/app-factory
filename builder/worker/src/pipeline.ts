508d0a1887366c992a0505fe5587c0d66d5d01ef
furkanbesirli <frknnnbesirli@gmail.com>
2026-03-12 12:16:19 +0300

    logo

--- builder/worker/src/pipeline.ts
+++ builder/worker/src/pipeline.ts
@@ -448,7 +448,9 @@ export async function executeBuild(ctx: BuildContext): Promise<{
       const adaptiveLauncherXml = `<?xml version="1.0" encoding="utf-8"?>
 <adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
     <background android:drawable="@color/ic_launcher_background" />
-    <foreground android:drawable="@drawable/ic_launcher_foreground" />
+    <foreground>
+        <inset android:inset="18%" android:drawable="@drawable/app_logo" />
+    </foreground>
 </adaptive-icon>`;
       fs.writeFileSync(path.join(anydpiDir, 'ic_launcher.xml'), adaptiveLauncherXml);
       fs.writeFileSync(path.join(anydpiDir, 'ic_launcher_round.xml'), adaptiveLauncherXml);
